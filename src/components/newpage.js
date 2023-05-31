5d9172769417ae6fc31995001eb54537045743c0
Mohamed kaif <ic22btech11005@iith.ac.in>
2023-05-31 22:39:37 +0530

    first comit in new branch

---
+++ src/components/newpage.js
@@ -0,0 +1,26 @@
+import { Fragment } from "react"
+import classes from "./newpage.module.css"
+function Frontpage(){
+    return(
+        <Fragment>
+        <header className={classes.headerImage}>
+            
+        </header>
+        <div className={classes.Content}>
+         </div>
+         <div className={classes.about}>
+         </div>
+         <div className={classes.Eduimage}>
+         </div>
+         <div className={classes.Footerimage}>
+         </div>
+         <div className={classes.font}>
+            <p>hfiaufhpauh</p>
+         </div>
+         <div>
+            dfahiu
+         </div>
+        </Fragment>
+    )
+}
+export default Frontpage
